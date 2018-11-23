b79900a0c7b22fd4e2f86426c8a15ae732e70018
Saad Ismail <saad3112@gmail.com>
2018-11-23 10:42:13 +0500

    add ERD, add schema .sql

---
+++ database_setup.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS `question_tag`;
+DROP TABLE IF EXISTS `vote`;
+DROP TABLE IF EXISTS `answer`;
+DROP TABLE IF EXISTS `question`;
+DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `f_name` varchar(20),
+  `l_name` varchar(20),
+  `email` varchar(30),
+  `password` varchar(30),
+  `access_level` tinyint,
+  `register_date` timestamp,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20),
+  `author_id` int,
+  FOREIGN KEY (author_id)
+    REFERENCES user(id),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `question` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(20),
+  `body` varchar(300),
+  `status` tinyint,
+  `author_id` int,
+  FOREIGN KEY (author_id)
+    REFERENCES user(id),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `answer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `body` varchar(300),
+  `author_id` int,
+  `question_id` int,
+  FOREIGN KEY (author_id)
+    REFERENCES user(id),
+  FOREIGN KEY (question_id)
+    REFERENCES question(id),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `question_tag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tag_id` int,
+  `question_id` int,
+  FOREIGN KEY (tag_id)
+    REFERENCES tag(id),
+  FOREIGN KEY (question_id)
+    REFERENCES question(id),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `vote` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int,
+  `question_id` int,
+  `flag` tinyint,
+  FOREIGN KEY (user_id)
+    REFERENCES user(id),
+  FOREIGN KEY (question_id)
+    REFERENCES question(id),
+  PRIMARY KEY (`id`)
+);
